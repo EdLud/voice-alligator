f826df7ddcf6f000f7f671601ea5ae79eb6b9819
edLud <edisludwig@gmail.com>
2025-08-19 04:56:41 +0200

    new workflow try 7

--- source/projects/voice-alligator/voice-alligator.cpp
+++ source/projects/voice-alligator/voice-alligator.cpp
@@ -2,7 +2,7 @@
 ///	@ingroup 	minexamples
 ///	@copyright	Copyright 2018 The Min-DevKit Authors. All rights reserved.
 ///	@license	Use of this source code is governed by the MIT License found in the License.md file.
-
+#include <algorithm>
 #include "c74_min.h"
 #include <vector>
 #include <queue>
